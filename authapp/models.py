b79fc9aab5aaf0ef5ba26a64678acd0fa53ba3f1
Shadow48lip <shome@lipsat.ru>
2022-01-27 18:09:59 +0300

    Fix error dump data

--- authapp/models.py
+++ authapp/models.py
@@ -12,7 +12,7 @@ class ShopUser(AbstractUser):
     age = models.PositiveIntegerField(verbose_name='возраст', default=18)
 
     activation_key = models.CharField(max_length=128, blank=True)
-    activation_key_expires = models.DateTimeField(blank=True)
+    activation_key_expires = models.DateTimeField(null=True, blank=True)
 
     def is_activation_key_expired(self):
         if now() <= self.activation_key_expires:
